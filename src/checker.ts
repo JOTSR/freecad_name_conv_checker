00c5628895f1b6df9942a528d32e1d214890f1db
Julien Oculi <xdrz.jot.sr@gmail.com>
2024-03-19 13:02:20 +0100

    refactor: :art: fmt

--- src/checker.ts
+++ src/checker.ts
@@ -3,7 +3,10 @@ import { Constraint } from '../types.ts'
 import { logCheckConstraint } from './logger.ts'
 import { Logger } from './logger.ts'
 
-export function checkConstraint(value: string, constraint: Constraint): boolean {
+export function checkConstraint(
+	value: string,
+	constraint: Constraint,
+): boolean {
 	const fields = constraint.pattern.match(/\$\w+/g) ?? []
 	const regexp = constraint.fields !== undefined
 		? fields.map((format) => constraint.fields![format.slice(1)])
