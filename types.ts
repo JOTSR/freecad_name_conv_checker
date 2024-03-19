ead689fb38e6aa744c2e38da0f894ba2435de04c
Julien Oculi <xdrz.jot.sr@gmail.com>
2024-03-19 14:53:26 +0100

    docs(api): :memo: add `jsdoc` for `Constraint` type

--- types.ts
+++ types.ts
@@ -1,3 +1,24 @@
+/**
+ * Name convention constraints.
+ *
+ * @field {string} kind is used to specify against what the constraint is made.
+ * @field {string} pattern correspond to the targeted token format. Use `$field` to define a custom field to check against a regexp.
+ * @field {object|undefined} fields define list of regexp to test against pattern fields. Order of the regexp matters.
+ *
+ * @example
+ * ```ts
+ * const snakeCaseLabel = /[a-z][a-z0-9_]*[a-z0-9]/
+ *
+ * const fileLabel: Constraint = {
+ * 	kind: 'file:label',
+ * 	pattern: '$label.$type',
+ * 	fields: {
+ * 		label: [snakeCaseLabel],
+ * 		type: [/valid/, /invalid/],
+ * 	},
+ * }
+ * ```
+ */
 export type Constraint = {
 	kind: 'file:path' | 'file:label' | 'object:label'
 	pattern: string
