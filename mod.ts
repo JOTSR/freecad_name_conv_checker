16bf6d1ff646ff45b12ff610a6d95d49a1a2a5b2
Julien Oculi <xdrz.jot.sr@gmail.com>
2024-03-20 10:45:49 +0100

    refactor(chore): :recycle: sort imports

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 import { Command } from '@cliffy/command/mod.ts'
-import config from './deno.json' with { type: 'json' }
 import { check } from './cli/check.ts'
 import { upgrade } from './cli/upgrade.ts'
+import config from './deno.json' with { type: 'json' }
 
 export { checkConstraint, checkConstraints } from './src/check.ts'
 export type { Constraint } from './types.ts'
