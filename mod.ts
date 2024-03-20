da01be318e59b83dd1d28776553c7eaabc7cd195
Julien Oculi <xdrz.jot.sr@gmail.com>
2024-03-20 10:39:40 +0100

    docs(cli): :memo: add main cli description

--- mod.ts
+++ mod.ts
@@ -7,8 +7,13 @@ export { checkConstraint, checkConstraints } from './src/checker.ts'
 export type { Constraint } from './types.ts'
 
 if (import.meta.main) {
+	const cliName = config.name.split('/')[1]
+
 	const cmd = new Command()
-		.name(config.name.split('/')[1])
+		.name(cliName)
+		.description(
+			`The simple and flexible FreeCAD name convention checker.\nUse "${cliName} <command> --help" for specific command help.`,
+		)
 		.version(config.version)
 		.command('check', check)
 		.command('upgrade', upgrade)
